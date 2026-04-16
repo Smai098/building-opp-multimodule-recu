69ea886209c2defddd3c5989fa90fceed6069b69
Crisstiian098 <smaielrisas@gmail.com>
2026-04-16 10:12:14 +0200

    Multimodule

---
+++ building-app-1/src/main/java/org/example/BuildingApp.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.example.readers.BuildingReader;
+
+import java.util.Scanner;
+
+public class BuildingApp {
+    private final Scanner scanner;
+    private final BuildingReader buildingReader;
+
+    public BuildingApp(Scanner scanner, BuildingReader buildingReader) {
+        this.scanner = scanner;
+        this.buildingReader = buildingReader;
+    }
+
+    public void run() {
+        var building = buildingReader.read();
+
+        System.out.println("Planta:");
+        int floor = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Puerta:");
+        String door = scanner.nextLine();
+
+        var apartment = building.findApartment(floor, door);
+        if (apartment == null) {
+            System.out.println("No se ha encontrado el apartamento");
+        } else {
+            apartment.showInfo();
+        }
+
+        building.showFloorApartments(2);
+    }
+}
